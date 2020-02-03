470e2c278cfdf1e2190e7a029b946b71205e01cf
Valery_S <shatten93@gmail.com>
2020-02-03 19:34:42 +0500

    Add get_absolute_url & __str__ methods

--- remember/models.py
+++ remember/models.py
@@ -1,6 +1,6 @@
 import uuid
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -24,3 +24,9 @@ class Remember(models.Model):
     title = models.CharField(max_length='200', help_text='Название воспоминания')
     description = models.TextField(max_length=2000, help_text='Описание воспоминания')
     remembers = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.id}, {self.title}'
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
