e2d1c8a1918243b29b43b054ba288414029b9628
Valery_S <shatten93@gmail.com>
2020-02-03 20:29:41 +0500

    Change ForeignKey to user in Remember model

--- remember/models.py
+++ remember/models.py
@@ -24,7 +24,7 @@ class Remember(models.Model):
 
     title = models.CharField(max_length=200, help_text='Название воспоминания')
     description = models.TextField(max_length=2000, help_text='Описание воспоминания')
-    remembers = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'{self.id}, {self.title}'
