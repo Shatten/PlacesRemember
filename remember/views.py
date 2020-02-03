eb88ce8991ca88d9442b77a3e73cc12a18e94523
Valery_S <shatten93@gmail.com>
2020-02-03 22:32:44 +0500

    Add login, user_remember, add_remember views

--- remember/views.py
+++ remember/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def index(request):
     return render(
         request,
         'index.html'
     )
+
+def login(request):
+    return render(request, 'login.html')
+
+@login_required
+def user_remembers(request):
+    return render(
+        request,
+        'user_remembers.html'
+    )
+
+@login_required
+def add_remember(request):
+    return render(
+        request,
+        'add_remember.html'
+    )
+
