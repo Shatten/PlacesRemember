467970cb55c86d2d44874622b4268e8bd92b50e3
Valery_S <shatten93@gmail.com>
2020-02-03 22:34:20 +0500

    Change remember url for login logic

--- remember/urls.py
+++ remember/urls.py
@@ -3,5 +3,8 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.login, name='login'),
+    url('remembers/', views.user_remembers, name='user_remembers'),
+    url('index/', views.index, name='index'),
+    url(r'^add_remember/$', views.add_remember, name='add_remember'),
 ]
