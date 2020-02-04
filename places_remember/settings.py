8967c0a083c63f642fb7ebfd54ed868a54bd047d
Valery_S <shatten93@gmail.com>
2020-02-04 16:13:57 +0500

    Test PostgreSQL

--- places_remember/settings.py
+++ places_remember/settings.py
@@ -93,6 +93,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -133,7 +135,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
