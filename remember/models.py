4fee2d0a70d4b240da5a79d53035257019327736
Valery_S <shatten93@gmail.com>
2020-02-04 15:25:25 +0500

    Complete indent refactoring

--- remember/models.py
+++ remember/models.py
@@ -2,6 +2,8 @@ import uuid
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+
+
 # Create your models here.
 
 
--- remember/views.py
+++ remember/views.py
@@ -5,6 +5,8 @@ from .models import Remember
 from django.views import generic
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
+
 # Create your views here.
 
 @login_required
@@ -14,8 +16,10 @@ def index(request):
         'index.html'
     )
 
+
 def login(request):
     return render(request, 'login.html')
+
 
 @login_required
 def user_remembers(request):
@@ -27,6 +31,7 @@ def user_remembers(request):
         context={'remembers_list': remembers_list}
     )
 
+
 @login_required
 def add_remember(request):
     if request.method == 'POST':
@@ -35,7 +40,6 @@ def add_remember(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.id_user = request.user
-            # post.map_coordinates = request.POST['map_coordinates']
             post.save()
         return HttpResponseRedirect(reverse('remembers'))
     else:
